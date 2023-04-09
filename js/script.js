20c7057393fb094bffc9d95c54ed391e8ca00012
Eugene <zheka.los2016@gmail.com>
2023-04-09 11:16:41 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict';
+import   tabs  from'./modules/tabs';
+import   modal, { openModal }  from'./modules/modal';
+import   timer  from'./modules/timer';
+import   cards  from'./modules/cards';
+import   calc  from'./modules/calc';
+import   forms  from'./modules/forms';
+import   slider  from'./modules/slider';
+
+window.addEventListener('DOMContentLoaded', () =>{
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 50000);
+      
+
+    tabs('.tabheader__item','.tabcontent','.tabheader__items','tabheader__item_active');
+    modal('[data-modal]','.modal', modalTimerId);
+    timer('.timer', '2023-06-11');
+    cards();
+    calc();
+    forms('.form',modalTimerId);
+    slider({
+        container:'.offer__slider',
+        nextArrow:'.offer__slider-next',
+        prevArrov:'.offer__slider-prev',
+        slide:'.offer__slide',
+        totalCounter:'#total',
+        currentCountr:'#current',
+        wrapper:'.offer__slider-wrapper',
+        field:'.offer__slider-inner'
+    });
+});
